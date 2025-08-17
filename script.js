d40a765b2da9d303071733474de6caa6b3f4cbaa
Bharti <110337668+Bharti025@users.noreply.github.com>
2025-08-17 20:04:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let ans=str[0];
+	for(let i=1; i<str.length; i++){
+      if(!includes(str[i])){
+		  ans+=str[i];
+	  }
+	}
+	return ans[0];
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
